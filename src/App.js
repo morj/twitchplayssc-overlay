8128c892bb348977c022454783bcf0128bed8444
Pavel Nikitin <pavel.nikitin@jetbrains.com>
2018-10-14 23:10:34 +0300

    get rid of inline styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './App.scss';
 
 // import {getInitialData} from './DataProvider';
 
@@ -13,7 +14,7 @@ class App extends Component {
         };
         this.eventSource = new EventSource('http://localhost:8082/persist');
         this.lastChatMessage = null;
-        // window.setInterval(() => this.pushData({text: 'xxxxxx'}), 2000);
+        // window.setInterval(() => this.pushData({text: 'this.eventSource = new EventSource(\'http://localhost:8082/persist\');'}), 2000);
     }
 
     pushData(added) {
@@ -52,74 +53,49 @@ class App extends Component {
     render() {
         let app = this;
         return (
-            <div className="App" style={{width: '100%', height: '100%', color: 'white'}}>
+            <div className="App">
                 {{
-                    compact: <div style={{
-                        position: 'absolute', left: 363, right: 407, top: 872, bottom: 11, background: 'black'
-                    }}>
-                        <div style={{
-                            float: 'left', width: '60%', height: '100%', lineHeight: '32px', marginTop: 12, marginLeft: 12,
-                        }}>
-                            <div style={{width: '100%', height: '50%'}}>
-                                <div style={{height: '50%', width: '20%', float: 'left', fontSize: 30}}>
-                                    <div style={{marginBottom: 8}}>
+                    compact: <div className="in-game-overlay">
+                        <div className="left-panel">
+                            <div className="stats">
+                                <div className="players">
+                                    <div className="item">
                                         Players {app.state.stats.players}
                                     </div>
-                                    <div style={{marginBottom: 8}}>
+                                    <div className="item">
                                         APM {app.state.stats.apm}
                                     </div>
                                 </div>
-                                <div style={{height: '50%', width: '60%', float: 'right', fontSize: 30}}>
-                                    <div style={{marginBottom: 8}}>
+                                <div className="games">
+                                    <div className="item">
                                         Games won {app.state.stats.gamesWon}, lost {app.state.stats.gamesLost}
                                     </div>
                                 </div>
                             </div>
-                            <div style={{width: '100%', height: '50%', fontSize: 26, color: 'orange'}}>
-                                NOTE: If !$ or !money does not work for you,<br/>try following the channel and refreshing the page
+                            <div className="rtfm">
+                                NOTE: If !$ or !money does not work for you,<br/>
+                                try following the channel and refreshing the page
                             </div>
                         </div>
-                        <div style={{
-                            float: 'right', height: '100%', width: '35%', overflow: 'auto', marginTop: 12
-                        }}>{
-                            app.state.data.map((item, index) => <div
-                                key={index}
-                                style={{paddingBottom: 4}}>
+                        <div className="right-panel chat">{
+                            app.state.data.map((item, index) => <div key={index} className="message">
                                 {item.text}
                             </div>)
                         }
-                            <div style={{
-                                paddingTop: 1, height: 1, width: 10, float: 'right', clear: 'both', background: 'white'
-                            }}
-                                 ref={(el) => {
-                                     this.lastChatMessage = el;
-                                 }}>
-                            </div>
+                            <div className="cursor" ref={(el) => {
+                                this.lastChatMessage = el;
+                            }}/>
                         </div>
                     </div>,
-                    full: <div style={{
-                        position: 'absolute', width: '100%', height: '100%', background: `rgba(0, 0, 0, 0.5)`
-                    }}>
-                        <div style={{
-                            float: 'right',
-                            height: '100%',
-                            width: '100%',
-                            overflow: 'auto',
-                            marginTop: 4
-                        }}>{
-                            app.state.data.map((item, index) => <div
-                                key={index}
-                                style={{paddingTop: 2, paddingBottom: 2}}>
+                    full: <div className="lobby-overlay">
+                        <div className="lobby-panel chat">{
+                            app.state.data.map((item, index) => <div key={index} className="message">
                                 {item.text}
                             </div>)
                         }
-                            <div style={{
-                                paddingTop: 1, height: 1, width: 10, float: 'right', clear: 'both', background: 'white'
-                            }}
-                                 ref={(el) => {
-                                     this.lastChatMessage = el;
-                                 }}>
-                            </div>
+                            <div className="cursor" ref={(el) => {
+                                this.lastChatMessage = el;
+                            }}/>
                         </div>
                     </div>
                 }[app.state.mode]}
